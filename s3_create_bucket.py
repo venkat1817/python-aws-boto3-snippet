07e8cf84f4a0526a898c69340f9ef035858aaee4
venkat <118958583+venkat1817@users.noreply.github.com>
2023-08-23 23:02:24 +0530

    Update s3_create_bucket.py

--- s3_create_bucket.py
+++ s3_create_bucket.py
@@ -12,3 +12,33 @@ bucket = resource.create_bucket(
     Bucket = bucket_name,
     CreateBucketConfiguration = location)
 print("Amazon S3 bucket has been created")
+
+
+
+
+
+
+#This code execute
+import boto3
+
+# Set your desired region
+region_name = 'ap-southeast-1'  
+
+# Initialize the S3 resource with the specified region
+s3_resource = boto3.resource('s3', region_name=region_name)
+
+# Set your desired bucket name
+bucket_name = 'dev-environment-data' 
+
+# Create the bucket using the specified configuration
+try:
+    bucket = s3_resource.create_bucket(
+        ACL='private',  
+        Bucket=bucket_name,
+        CreateBucketConfiguration={
+            'LocationConstraint': region_name  
+        }
+    )
+    print("Amazon S3 bucket has been created")
+except Exception as e:
+    print("Error creating Amazon S3 bucket:", e)
